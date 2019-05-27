4ae9ce304e0fde5374483b60264429bac76e2d5a
Gennadiy Filatov <kain.winterheart@gmail.com>
2019-05-27 16:32:29 +0300

    first working draft!

--- src/handlers/v1/file/new.cpp
+++ src/handlers/v1/file/new.cpp
@@ -2,16 +2,33 @@
 #include <models/id.hpp>
 #include <models/files.hpp>
 
+#ifdef RELEASE_FILESYSTEM
+#include <filesystem>
+#else
+#include <experimental/filesystem>
+
+namespace std {
+    namespace filesystem = std::experimental::filesystem;
+}
+#endif
+
 namespace NAC {
     void TFileV1NewHandler::Handle(
         const std::shared_ptr<TFSMetaDRequest> request,
         const std::vector<std::string>& args
     ) {
         const auto& json = request->Json();
+        auto path = json["path"].get<std::string>();
+
+        if (!std::filesystem::is_regular_file(path)) {
+            request->Send400();
+            return;
+        }
+
         TFilesData data;
         data
             .SetName(json["name"].get<std::string>())
-            .SetPath(json["path"].get<std::string>())
+            .SetPath(path)
             .SetCType(json["ctype"].get<std::string>())
             .SetSize(0)
             .SetOffset(0)
